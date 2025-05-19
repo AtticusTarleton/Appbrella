31284d19b88ae377c98e1debc52dd605906fdb5c
AtticusTarleton <atarleto@uvm.edu>
2025-05-19 19:03:37 -0400

    created database, did testing

---
+++ makeSQLfile.py
@@ -0,0 +1,16 @@
+## This is the file creates the database for keeping track of the weather
+
+
+# Importing necessary packages
+import sqlite3
+
+# Connect to the database and create the cursor
+con = sqlite3.connect('weather.db', isolation_level = None)
+cur = con.cursor()
+
+# Remove table during subsequent runs
+cur.execute("DROP TABLE IF EXISTS WeatherPredictions;")
+
+# Create WeatherPredictions table
+create_table_super = '''CREATE TABLE WeatherPredictions('date', 'guess_made', 'city', 'country')'''
+cur.execute(create_table_super)
---
+++ test.py
@@ -0,0 +1,30 @@
+import socket
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
+print("Your Computer Name is:" + hostname)
+print("Your Computer IP Address is:" + IPAddr)
+
+import public_ip as ip
+
+print(ip.get())
+public_IPAddr = ip.get()
+
+
+
+import ipinfo
+
+
+access_token = input('please enter your access token: ') # to get your own access token, go to
+#https://ipinfo.io/dashboard and sign up. you get 50,000 requests on the free plan each month
+
+ip_address = public_IPAddr  # Replace with the desired IP address
+
+
+import ipinfo
+handler = ipinfo.getHandler(access_token=access_token)
+details = handler.getDetails(ip_address = public_IPAddr)
+print(details.all)
+print(details.city)
+print(details.country_name)
+print(details.loc)
